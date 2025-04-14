4eea42c8a14e6b43947bf77823201f3c1f08207d
Sebastian Kootz <skxxtz@gmail.com>
2025-04-14 23:51:40 +0200

    chore: added LICENSE and packager script

---
+++ packager.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+read -p "Current version: " version
+rm -rf ~/.tmp/soccer-release/
+mkdir -p ~/.tmp/soccer-release/
+cargo build --release
+cp target/release/soccer ~/.tmp/soccer-release/sherlock
+cp LICENSE ~/.tmp/soccer-release/LICENSE
+
+cd ~/.tmp/soccer-release/
+tar -czf soccer-v${version}-bin-linux-x86_64.tar.gz soccer LICENSE
+

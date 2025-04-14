944bbab5bc5cabd7fb2bfff93645adca4793f129
Sebastian Kootz <skxxtz@gmail.com>
2025-04-14 23:53:20 +0200

    fix: fixed incorrect filename in packager script

--- packager.sh
+++ packager.sh
@@ -4,7 +4,7 @@ read -p "Current version: " version
 rm -rf ~/.tmp/soccer-release/
 mkdir -p ~/.tmp/soccer-release/
 cargo build --release
-cp target/release/soccer ~/.tmp/soccer-release/sherlock
+cp target/release/soccer ~/.tmp/soccer-release/soccer
 cp LICENSE ~/.tmp/soccer-release/LICENSE
 
 cd ~/.tmp/soccer-release/
